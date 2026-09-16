2887fbf9d7534fe97a3c9f7d592d93b81e9da2bf
zudww <zudwduz@gmail.com>
2026-09-16 23:06:32 +0800

    update: make `nixllization` input follow `nixlib-general`

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "nixstrata-transforms";
 
   inputs.nixlib-general.url = "git+https://github.com/zudww/nixlib-general?ref=v0.3.0-a1";
+
   inputs.nixllization.url = "git+https://github.com/snowline-nix/nixllization?ref=v0.1.0-a3";
+  inputs.nixllization.inputs.nixlib-general.follows = "nixlib-general";
 
   outputs = _: import ./src _;
 }
