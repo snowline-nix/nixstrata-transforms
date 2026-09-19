45703a9584f478ad13eebb290780911725fd7859
zudww <zudwduz@gmail.com>
2026-09-19 23:35:10 +0800

    update(errors): revamp error messages

--- src/errors.nix
+++ src/errors.nix
@@ -1,81 +1,89 @@
-{ toRepr, ... }:
+{ toRepr, remapElems, joinStringsSep, ... }:
 let
-  getDecls = context:
-    if context.module != null
-    then "\nThe definition was evaluated at:\n\t${context.module.source}\n"
-    else "";
+  getPath = path:
+    builtins.substring 1 (-1)
+    (
+      joinStringsSep ""
+      (remapElems path (
+        { type, address }:
+          if type == "attribute" then ".${address}"
+          else if type == "element" then "[${address}]"
+          else ".({${type}.${address})"
+      ))
+    );
+
+  mkModuleError = { transform, context, content, summary, pathInSummary ? true }: let
+    hasPath = context ? path;
+    path = getPath context.path;
+    summary' = summary + (if pathInSummary && hasPath then " at `${path}`" else "");
+    at = (if context ? module then "\nIn: ${context.module.sourcePath}\n" else "") + (if hasPath then "\nAt: `${path}`\n" else "");
+  in
+    throw ''
+      ${summary'}
+      While evaluating: '${transform.identifier}'
+      ${at}
+      ${content { inherit hasPath path; }}
+
+      error: ${summary'}
+    '';
 in
 {
-  noValueErr = { context, transform, ... }: let
-    path = if context.path != null then " for `${context.path}`" else "";
-    summary = "no value was declared${path}";
-  in
-    throw ''
-      ${summary}
-      ${getDecls context}
+  noValueErr = { context, transform, ... }: mkModuleError {
+    inherit context transform;
+    summary = "no value was declared";
+    content = { hasPath, path, ... }: ''
       ${
-        if context.type != null
-        then "This type requires a value to evaluate.\n\nType:\n\t${context.type.name}"
-        else "This transform step (`${transform.identifier}`) requires a value to evaluate"
+        if context ? type
+        then "Type (${context.type.name}) requires a value to evaluate."
+        else "Transform step ('${transform.identifier}') requires a value to evaluate"
       }
 
-      No values were declared${path}.
-
-      error: ${summary}
+      No value was declared for one of the declarations${if hasPath then " of ${path}" else ""}.
     '';
+  };
 
   # Requires `context.type` to be a type.
-  typeCheckErr = { context, valueObj, ... }: let
-    hasPath = context.path != null;
+  typeCheckErr = { context, valueObj, transform, ... }: let
     type = context.type;
-    summary = "type mismatch `${type.name}`${if hasPath then " at `${context.path}`" else ""}";
     value = toRepr valueObj.value;
-  in
-    throw ''
-      ${summary}
-      ${getDecls context}
+  in mkModuleError {
+    inherit context transform;
+    summary = "invalid value for type '${type.name}'";
+    content = _: ''
       This type requires a value conforming to:
 
-      `${type.name}`${if type.description != null then " - ${type.description}" else ""}
+      '${type.name}'${if type ? description then " - ${type.description}" else ""}
 
-      The value `${value}` does not conform to the${if context.option != null then " option" else ""} type `${type.name}`.
-      ${
-        if hasPath then "At: `${context.path} = ${value}`" else ""
-      }
-      error: ${summary}
-    '';
+      Full type: ${type.fullDefinitionName}
 
-  mergeErr.missingDeclarationsErr = { context, transform, ... }: let
-    hasPath = context.path != null;
-    path = context.path;
-    summary = "no value declarations provided for merging${if hasPath then " at `${path}`" else ""}";
-  in
-    throw ''
-      ${summary}
-      ${getDecls context}
-      ${
-        if context.type != null
-        then "Type ${context.type.name} has no values to work with."
-        else "Merge method ${transform.identifier} has no values to work with."
-      }
-      ${if hasPath then "\nAt: `${path}`\n" else ""}
-      error: ${summary}
+      The value `${value}` does not conform to the${if context ? option then " option" else ""} type '${type.name}'.
     '';
+  };
+
+  mergeErr.missingDeclarationsErr = { context, transform, ... }: mkModuleError {
+    inherit context transform;
+    summary = "no values were declared";
+    content = { hasPath, path, ... }: ''
+      No values were declared${if hasPath then " for ${path}" else ""}.
+    '';
+  };
 
   mergeErr.mergingUnsupportedErr = { context, transform, ... }: let
-    hasPath = context.path != null;
-    path = context.path;
-    summary = "type ${context.type.name} does not support merging";
-  in
-    throw ''
-      ${summary}
-      ${getDecls context}
-      ${
-        if context.type != null
-        then "Type ${context.type.name} does not support merging of multiple value declarations."
-        else "Merge method `${transform.identifier}` does not support merging of multiple values."
-      }
-      ${if hasPath then "\nAt: `${path}`\n" else ""}
-      error: ${summary}
+    typeName = context.type.name;
+    transformId = transform.identifier;
+    genMsg = capitalized:
+      if context ? type
+      then "${if capitalized then "T" else "t"}ype '${typeName}'"
+      else "${if capitalized then "M" else "m"}erge method '${transformId}'"
+      ;
+  in mkModuleError {
+    inherit context transform;
+    pathInSummary = false;
+    summary = (genMsg false) + "does not support merging";
+    content = { hasPath, path, ... }: ''
+      ${genMsg true} does not support merging multiple value declarations.
+
+      No values were declared${if hasPath then " for ${path}" else ""}.
     '';
+  };
 }
